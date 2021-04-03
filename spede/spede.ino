3786ee088ff260b50fd7aded70d5803386574bb1
Ross <rosshaydenkelly@gmail.com>
2021-04-03 08:42:19 +0200

    banking content before adding more games

--- spede/spede.ino
+++ spede/spede.ino
@@ -106,7 +106,7 @@ void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("hello, world!");
+  lcd.print("Test your Skillz!");
   
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
